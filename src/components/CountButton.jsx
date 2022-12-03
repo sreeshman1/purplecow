c5fc6b71884e39bd5cc9f1b5e8077664d245953d
Sreesh Manickababu <sreeshman1@gmail.com>
2022-12-02 16:25:10 -0800

    disable button when request is being sent to api

--- src/components/CountButton.jsx
+++ src/components/CountButton.jsx
@@ -4,21 +4,25 @@ import CountApiService from '../services/countApiService';
 function CountButton() {
     const countService = new CountApiService();
     const [count, setCount] = useState(0);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
 
     useEffect(() => {
         countService.getCount().then((result) => {
             setCount(result.value);
+            setButtonDisabled(false);
         })
     }, [setCount]);
 
     const onCountButtonClick = useCallback(() => {
+        setButtonDisabled(true);
         countService.updateCount(1).then((result) => {
             setCount(result.value);
+            setButtonDisabled(false);
         })
     }, [setCount]);
 
     return (
-        <button className="count-button" onClick={onCountButtonClick}>{count}</button>
+        <button disabled={buttonDisabled} className="count-button" onClick={onCountButtonClick}>{count}</button>
     )
 }
 
